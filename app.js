5923da4187b8c767c12db3e07b498de60a2d2535
Abhijeet Bharti <86771483+printabhijeet@users.noreply.github.com>
2024-06-10 09:12:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const { ndown } = require("nayan-media-downloader")
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    return res.render("index");
+});
+
+
+app.get("/download", async function (req, res) {
+
+    const links = await ndown(req.query.url);
+
+    console.log(links.data[0].url);
+
+    res.render("download", { url: links.data[0].url, video: req.query.url });
+});
+
+app.get("/ads.txt", (req, res) => {
+    return res.sendFile("/ads.txt")
+});
+
+app.get("/robots.txt", (req, res) => {
+    return res.sendFile("/robots.txt")
+});
+
+app.get("/sitemap.xml", (req, res) => {
+    return res.sendFile("/sitemap.xml")
+});
+
+
+app.listen(port, function () {
+    console.log("Server is running ");
+});
+
+
+
+
